bc89c570d052c118fba3e4d8248e6ff19b537bf8
Cedricaf <34737@ma-web.nl>
2024-12-16 14:33:49 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css"; // Import de CSS-bestanden voor styling
+
+function Navigation() {
+  return (
+    
+    <nav className="navigation">
+      <div className="logo">
+        <img
+          src="GemeenteAmsterdam.svg"
+          alt="logo gemeente Amsterdam"
+        />
+      </div>
+      <ul>
+        <li className="active">
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Agenda</a>
+        </li>
+        <li>
+          <a href="#">Organisatoren</a>
+        </li>
+        <li>
+          <a href="#">Ondernemers</a>
+        </li>
+        <li>
+          <a href="#">Bewoners</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navigation />
+      <section className="hero"></section>
+      <section className="information"></section>
+    </main>
   );
 }
 
