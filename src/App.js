c1d03169183f231ef7891a7ab892e16f99d322d1
Cedricaf <34737@ma-web.nl>
2025-01-21 19:28:18 +0100

    m

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function Information1() {
         <p>
           Bij het Projectmanagementbureau verzorgen vakmensen het project-, programma- en procesmanagement in de stad. We doen dat voor sociale, fysieke en economische projecten en programma’s. Daarnaast ontwikkelen wij samen met opdrachtgevers en maatschappelijke partners nieuwe kennis en werkwijzen die passen bij de vraagstukken van vandaag en morgen. Onze opdrachtgevers zijn: stad, stadsdelen, wijken, buurten en de regio.
         </p>
-        <button className="read-more-button">Lees meer</button>
+        
       </div>
       <div className="rightside">
         <img src={image2} alt="zwart-wit foto van een stad van bovenaf" />
@@ -77,11 +77,11 @@ function Information2() {
         <img src={image3} alt="Roeiboot op zee met uitzicht op de brug van San Fransisco" />
       </div>
       <div className="leftside">
-        <h1>Het Projectmanagementbureau</h1>
+        <h1>Werken bij Projectmanagementbureau</h1>
         <p>
-          Bij het Projectmanagementbureau verzorgen vakmensen het project-, programma- en procesmanagement in de stad. We doen dat voor sociale, fysieke en economische projecten en programma’s. Daarnaast ontwikkelen wij samen met opdrachtgevers en maatschappelijke partners nieuwe kennis en werkwijzen die passen bij de vraagstukken van vandaag en morgen. Onze opdrachtgevers zijn: stad, stadsdelen, wijken, buurten en de regio.
+        Naast ca 500 medewerkers in vaste dienst heeft het Projectmanagementbureau een flexibele schil om pieken in het werk goed te kunnen opvangen. We huren regelmatig junior projectmanagers en managementondersteuning in via uitzendbureaus. De gemeente Amsterdam doet zaken met drie uitzendbureaus: Randstad, Tempo Team en Olympia. Heb je interesse? Neem dan vooral een kijkje op hun websites.
         </p>
-        <button className="read-more-button">Lees meer</button>
+        
       </div>
 
     </section>
